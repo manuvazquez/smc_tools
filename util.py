97605d501e01eb25358404ba117b1c0664391e9f
Manuel A. Vázquez <manuavazquez@gmail.com>
2016-09-22 20:47:32 +0200

    - added smc/util.py

---
+++ util.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def log_sum_from_individual_logs(logs):
+
+	descending_sort = np.sort(logs)[::-1]
+
+	return descending_sort[0] + np.log1p(np.exp(descending_sort[1:] - descending_sort[0]).sum())
+
+
+def normalize_from_logs(logs):
+
+	log_sum = log_sum_from_individual_logs(logs)
+
+	return np.exp(logs - log_sum)
